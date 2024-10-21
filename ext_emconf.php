0bf8de39a601376806c54ff7dd069dcfbea034a8
Michael Lang <113889180+t3ac@users.noreply.github.com>
2024-10-21 20:37:05 +0200

    Add files via upload

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title'            => 'T3UP - Table',
     'description'      => 'Table Extension for the t3up-package',
-    'version'          => '1.0.0',
+    'version'          => '11.0.0a',
     'state'            => 'stable',
     'category'         => 'templates',
     'author'           => 'Michael Lang',
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => true,
     'constraints'      => [
         'depends'   => [
-            'typo3' => '11.0-11.5',
+            'typo3' => '11.5.0-11.5.99',
         ],
         'conflicts' => [
             'fluidpages' => '*',
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,19 @@
 <?php
 
 /*
- * This file is part of the package t3up-package.
+ * This file is part of the package T3UP.
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
  */
+declare(strict_types=1);
 
-defined('TYPO3_MODE') or die();
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:t3up_table/Configuration/TsConfig/TCEFrames.typoscript">');    
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-?>
+call_user_func(static function () {
+
+	ExtensionManagementUtility::addPageTsConfig(
+		"@import 'EXT:t3up_table/Configuration/TsConfig/TCETables.tsconfig'"
+    );
+});
+	
