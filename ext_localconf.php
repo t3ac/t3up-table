e86aae92b1719b8c106f6113fb5c12a6c2f0a55b
Michael Lang <113889180+t3ac@users.noreply.github.com>
2024-11-16 17:32:52 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,19 +1,16 @@
 <?php
 
-/*
- * This file is part of the package T3UP.
- *
- * For the full copyright and license information, please read the
- * LICENSE file that was distributed with this source code.
- */
 declare(strict_types=1);
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 call_user_func(static function () {
-
 	ExtensionManagementUtility::addPageTsConfig(
 		"@import 'EXT:t3up_table/Configuration/TsConfig/TCETables.tsconfig'"
-    );
+    	);
+    	ExtensionManagementUtility::addTypoScript(
+       		't3up-table',
+        	'setup',
+       	 	"@import 'EXT:t3up-table/Configuration/TypoScript/setup.typoscript'",
+    	);
 });
-	
