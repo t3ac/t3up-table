0f9d31b6e4380526e8ecf2db221916989bb7e2f9
Michael Lang <113889180+t3ac@users.noreply.github.com>
2024-11-25 10:23:27 +0100

    Update sys_template.php

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -4,6 +4,6 @@ defined('TYPO3_MODE') || defined('TYPO3') || die('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
 	't3up_table', 
-	'Configuration/TypoScript', 
+	'Configuration/TypoScript/Setup', 
 	'T3UP - Table-Setup'
 	);
